7b52e9c39f977418ca53ea963bb4f0b1b58fa2ab
Neal-Leo301910 <113656016+Neal-Leo301910@users.noreply.github.com>
2025-03-28 21:08:18 +1100

    Transformation_Matrix_from_Basis_B_to_C.py

--- Transformation_Matrix_from_Basis_B_to_C.py
+++ Transformation_Matrix_from_Basis_B_to_C.py
@@ -1,7 +1,8 @@
 '''
 P = C^{-1} * B
+Determinant 使用Numpy的linalg.det(matrix)function可直接求解
 inverse matric = adj{C} / det{C}, 使用Numpy的linalg.inv() function可直接求解
-
+adjugate matrix Numpy无直接的 function求解，需要从上面两个function中得到答案后求解
 '''
 import numpy as np
 
