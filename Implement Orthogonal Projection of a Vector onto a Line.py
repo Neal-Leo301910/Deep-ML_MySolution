6ededffcca1915e249497d1c93045cd0ee82306c
Neal-Leo301910 <113656016+Neal-Leo301910@users.noreply.github.com>
2025-03-29 13:43:22 +1100

    Create_Implement_Orthogonal_Projection_of_a_Vector_onto_a_Line.py
    Linear Algebra

---
+++ Implement Orthogonal Projection of a Vector onto a Line.py
@@ -0,0 +1,24 @@
+'''
+project_L(v) = (v dot L) divide (L dot L) dot L = (v * unit vector L^) dot unit vector L^
+'''
+import numpy as np
+
+def orthogonal_projection(v, L):
+	"""
+	Compute the orthogonal projection of vector v onto line L.
+
+    :param v: The vector to be projected
+	:param L: The line vector defining the direction of projection
+	:return: List representing the projection of v onto L
+	"""
+    proj_vec = np.dot(np.divide(np.dot(v,L),np.dot(L,L)),L)
+    return [round(float(x), 3) for x in proj_vec]
+
+v = [3, 4]
+L = [1, 0]
+print(orthogonal_projection(v, L))
+    
+    
+
+
+
