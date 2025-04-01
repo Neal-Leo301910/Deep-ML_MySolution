06181672056d7e7ad1a70428aa4070a1fb654578
Neal-Leo301910 <113656016+Neal-Leo301910@users.noreply.github.com>
2025-04-01 21:13:44 +1100

    Create 2D Translation Matrix Implementation.py
    Linear Algebra(Medium)

---
+++ 2D Translation Matrix Implementation.py
@@ -0,0 +1,28 @@
+'''
+Your task is to implement a function that applies a 2D translation matrix to a set of points. 
+A translation matrix is used to move points in 2D space by a specified distance in the x and y directions.
+Write a function translate_object(points, tx, ty) 
+where points is a list of [x, y] coordinates and tx and ty are the translation distances in the x and y directions, respectively.
+The function should return a new list of points after applying the translation matrix.
+'''
+
+
+import numpy as np
+def translate_object(points, tx, ty):
+'''
+
+translated_points = [x+x_t, y+y_t]
+'''
+    trans_arr = [tx,ty]
+
+    translated_points = []
+
+    for point in points:
+        translated_points.append([point[0]+trans_arr[0],point[1]+trans_arr[1]])
+    
+    return translated_points
+
+points = [[0, 0], [1, 0], [0.5, 1]]
+tx, ty = 2, 3
+
+print(translate_object(points, tx, ty))
