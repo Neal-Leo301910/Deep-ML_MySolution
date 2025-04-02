8b487c90b323d7908df94cb77a2f9ebb39e2a50a
Neal-Leo301910 <113656016+Neal-Leo301910@users.noreply.github.com>
2025-04-02 21:18:24 +1100

    Update Transpose_of_a_Matrix.py

--- Linear Algbra/Easy/Transpose_of_a_Matrix.py
+++ Linear Algbra/Easy/Transpose_of_a_Matrix.py
@@ -1,16 +1,19 @@
-‘’‘Write a Python function that computes the transpose of a given matrix.‘’‘
+‘’‘
+Write a Python function that computes the transpose of a given matrix.
+转置矩阵就是矩阵行index和列index交换位置
+‘’‘
 
 def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
 
-    b =  [[0 for j in range(len(a))] for i in range(len(a[0]))]
+    b = [ [0 for j in range(len(a))] for i in range(len(a[0])) ]
     
-    for j in range(len(a[0])):
-        for i in range(len(a)):
-            b[j][i] = a[i][j]
+    for row in range(len(a)):
+        for col in range(len(a[0])):
+            b[col][row] = a[row][col]
+    
     return b
 
+print(transpose_matrix([[1,2],[3,4],[5,6]]))
+print(transpose_matrix([[1,2,3],[4,5,6]]))
 
-
-transpose_matrix([[1,2,3],[4,5,6]])
-transpose_matrix([[1,2],[3,4],[5,6]])
 
