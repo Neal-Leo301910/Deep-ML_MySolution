d2d940fde36fae9bf22ca7493470b50d1f3a91d2
Neal-Leo301910 <113656016+Neal-Leo301910@users.noreply.github.com>
2025-04-05 14:15:47 +1100

    Update and rename Calculate Cosine Similarity Between Vectors.py to calculate_cosine_similarity_between_vectors.py

--- calculate_cosine_similarity_between_vectors.py
+++ calculate_cosine_similarity_between_vectors.py
@@ -27,8 +27,15 @@ def cosine_similarity(v1, v2):
     else:
         return np.around(np.divide(np.dot(v1,v2),Mag_v1*Mag_v2),3)
 
-import numpy as np
 
 v1 = np.array([1, 2, 3])
 v2 = np.array([2, 4, 6])
+print(cosine_similarity(v1, v2))
+
+v1 = np.array([1, 2, 3]) 
+v2 = np.array([-1, -2, -3]) 
+print(cosine_similarity(v1, v2))
+
+v1 = np.array([1, 0, 7]) 
+v2 = np.array([0, 1, 3]) 
 print(cosine_similarity(v1, v2))
