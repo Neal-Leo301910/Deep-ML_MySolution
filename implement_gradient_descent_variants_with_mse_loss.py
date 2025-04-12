8ad0100d42cd5dc1876d18a208d99dad0d21813d
Neal-Leo301910 <113656016+Neal-Leo301910@users.noreply.github.com>
2025-04-12 13:39:38 +1000

    Update implement_gradient_descent_variants_with_mse_loss.py

--- implement_gradient_descent_variants_with_mse_loss.py
+++ implement_gradient_descent_variants_with_mse_loss.py
@@ -29,3 +29,32 @@ def gradient_descent(X, y, weights, learning_rate, n_iterations, batch_size=1, m
                 weights = weights - learning_rate * gradient
 
     return weights
+
+
+X = np.array([[1, 1], [2, 1], [3, 1], [4, 1]]) 
+y = np.array([2, 3, 4, 5]) 
+weights = np.zeros(X.shape[1]) 
+learning_rate = 0.01 
+n_iterations = 100 
+# Test Batch Gradient Descent 
+output = gradient_descent(X, y, weights, learning_rate, n_iterations, method='batch') 
+print(output)
+
+X = np.array([[1, 1], [2, 1], [3, 1], [4, 1]]) 
+y = np.array([2, 3, 4, 5]) 
+weights = np.zeros(X.shape[1])
+learning_rate = 0.01 
+n_iterations = 100 
+# Test Stochastic Gradient Descent 
+output = gradient_descent(X, y, weights, learning_rate, n_iterations, method='stochastic') 
+print(output)
+
+X = np.array([[1, 1], [2, 1], [3, 1], [4, 1]]) 
+y = np.array([2, 3, 4, 5]) 
+weights = np.zeros(X.shape[1]) 
+learning_rate = 0.01 
+n_iterations = 100 
+batch_size = 2 
+# Test Mini-Batch Gradient Descent 
+output = gradient_descent(X, y, weights, learning_rate, n_iterations, batch_size, method='mini_batch') 
+print(output)
